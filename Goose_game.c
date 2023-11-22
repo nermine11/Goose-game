008ce75e841e63af6973f9226833b03a7bde8d51
M.petrov <michael.petrov@edu.univ-eiffel.fr>
2023-11-22 16:17:15 +0100

    💚

--- Goose_game.c
+++ Goose_game.c
@@ -126,31 +126,24 @@ void collision(char plateau[], int positions[], int attente[], int nb_joueurs,
 
 int avancerJoueur(char plateau[], int positions[], int attente[], int joueur_courant, int nb_joueurs, int des[2], int premier_tour)
 {
-
-  // si le joueur est dans la case hotel et il lui reste 1 tour pour sortir
-  if (attente[joueur_courant - 1] == 2)
-    attente[joueur_courant - 1] = 1;
-
-  // si le joueur est dans la case hotel et il lui reste 0 tour pour sortir
-  if (attente[joueur_courant - 1] == 1)
-    attente[joueur_courant - 1] = 0; // attente[0] correspond a joueur 1
-
+  if(attente[joueur_courant]){attente[joueur_courant]-=1;}
   if (!attente[joueur_courant - 1])
   {
     int nouvelle_pos;
-
-    if (premier_tour)
+    int cas_spes = 0;
+    if (premier_tour){
     {
       if ((des[0] == 4 && des[1] == 5) || (des[1] == 4 && des[0] == 5)) // cas special 4,5
-        nouvelle_pos = 89;
-      else if ((des[0] == 3 && des[1] == 6) || (des[1] == 3 && des[0] == 6)) // cas special 3,6
-        nouvelle_pos = 40;
+        nouvelle_pos = 89;cas_spes=1;}
+      if ((des[0] == 3 && des[1] == 6) || (des[1] == 3 && des[0] == 6)){ // cas special 3,6
+        nouvelle_pos = 40;cas_spes=1;}
       collision(plateau, positions, attente, nb_joueurs, joueur_courant, nouvelle_pos, des);
       positions[joueur_courant - 1] = nouvelle_pos;
     }
-    else
+    if(!cas_spes)
     {
       nouvelle_pos = positions[joueur_courant - 1] + des[0] + des[1];
+      printf("nouvellepos : %d\n",nouvelle_pos);
       if (nouvelle_pos > 99) // si on depase 100
         nouvelle_pos = 99 - nouvelle_pos % 99;
       collision(plateau, positions, attente, nb_joueurs, joueur_courant, nouvelle_pos, des);
@@ -167,7 +160,7 @@ int avancerJoueur(char plateau[], int positions[], int attente[], int joueur_cou
         return -1;
       }
     }
-  }
+  }else{printf("le joueur attend son tour !\n");}
   return -1;
 }
 
@@ -286,7 +279,7 @@ int main()
 
   char plateau[TAILLE_PLAT];
   creer_plateau(plateau);
-  int nb_joueurs;
+  int nb_joueurs = 2;
   int des[2];
   // creation du fichier ma_sauvegarde.jo
   FILE *fileW;
@@ -305,25 +298,24 @@ int main()
     // charger_fichier() // fonctionalité pas terminé
   } */
 
-  printf("Combien de joueurs ? "); // nb_joueurs
+  printf("Combien de joueurs ?"); // nb_joueurs
   scanf("%d", &nb_joueurs);
   while (nb_joueurs < 2 || nb_joueurs > 4)
   {
-    printf("Le nombre de joueurs est entre 2 et 4 ");
+    printf("Le nombre de joueurs doit etre entre 2 et 4\n");
     scanf("%d", &nb_joueurs);
   }
   //afficherPlateau( plateau, positions,nb_joueurs);
   // intialisation des tab positions[] et attente[]
-  int positions[nb_joueurs];
-  int attente[nb_joueurs];
-  for (int i = 0; i < nb_joueurs; i++)
-  {
-    positions[i] = 0;
-    attente[i] = 0;
-  }
+  int positions[4] = {};    // on met 4 pour ne pas initialisé par une variable et de toute fason le max possible est 4
+  int attente[4] = {};       //les listes sont initalisé a 0
   
   fprintf(fileW, "%d JO", nb_joueurs);
   printf("Pour chaque tour, indiquer les valeurs des deux des ou taper q pour quitter\n");
+
+
+
+
   // debut du jeu
   while (1)
   {
@@ -332,7 +324,7 @@ int main()
     for (joueur_courant = 1; joueur_courant <= nb_joueurs; joueur_courant++)
     {
       printf("Joueur %d: ", joueur_courant);
-      scanf("%d %d", des, des + 1);
+      //scanf("%d %d", des, des + 1);
 
       if (des[0] == -1) // on quitte quand le joueur tappe -1 pas q (input buffer erreur engendrée par q)
       {
@@ -349,7 +341,8 @@ int main()
       printf("%d %d\n", des[0], des[1]);
       fprintf(fileW, "%d %d\n", des[0], des[1]);
       avancerJoueur(plateau, positions, attente, joueur_courant, nb_joueurs, des, premier_tour);
-      // afficherPlateau( plateau, positions, nb joueurs);
+      afficherPlateau( plateau, positions, nb_joueurs,joueur_courant);
+      printf("positions : %d   %d",positions[0],positions[1]);
     }
 
     premier_tour = 0;
